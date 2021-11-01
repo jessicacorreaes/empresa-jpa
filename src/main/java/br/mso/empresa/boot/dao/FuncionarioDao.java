0ac1587bd521e62164b6556528b5112c443cc49b
jessicacorreaes <jessicacorreaes@gmail.com>
2021-11-01 19:34:20 -0300

    primeiro commit

---
+++ src/main/java/br/mso/empresa/boot/dao/FuncionarioDao.java
@@ -0,0 +1,17 @@
+package br.mso.empresa.boot.dao;
+
+import br.mso.empresa.boot.domain.Funcionario;
+import java.util.List;
+
+public interface FuncionarioDao {
+
+    void save(Funcionario funcionario);
+
+    void update(Funcionario funcionario);
+
+    void delete(Long id);
+
+    Funcionario findById(Long id);
+
+    List<Funcionario> findAll();
+}
